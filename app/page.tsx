0724694351e97d173ecd95b52d0a511fbb7d1188
Aman Yadav <ay.work07@gmail.com>
2025-09-07 13:20:15 +0530

    fix: ui-bug

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,20 @@
 "use client";
-import { Container } from "@/components/Container";
-import OnBoarding from "@/components/onboarding/onBoarding";
+import { useEffect } from "react";
 import { useRouter } from "next/navigation";
 
 export default function Home() {
-  const router = useRouter()
-  const hasSeed = localStorage.getItem("seedPhrase");
-  console.log('LOCALSTORAGE SEED: ', hasSeed)
-  if (hasSeed) {
-    router.push('/dashboard')
-  } else {
-    router.push('/onboarding')
-  }
-  return (
-    <div className="font-sans  w-full flex justify-center bg-black">
-      <Container>
-        <OnBoarding/>
-      </Container>
-    </div>
-  );
+  const router = useRouter();
+
+  useEffect(() => {
+    const hasSeed = localStorage.getItem("seedPhrase");
+    console.log("LOCALSTORAGE SEED: ", hasSeed);
+
+    if (hasSeed) {
+      router.push("/wallet");
+    } else {
+      router.push("/onboarding");
+    }
+  }, [router]);
+
+  return null;
 }
---
+++ app/wallet/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div className='w-full pt-25 flex justify-center'>
+        <div className='text-white max-w-md min-h-screen'>
+          hello
+        </div>
+    </div>
+  )
+}
+
+export default page
